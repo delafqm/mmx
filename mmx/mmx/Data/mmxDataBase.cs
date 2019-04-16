c36df4c7c264507e190027eb693b387aa28f02ca
delafqm <delafqm@163.com>
2019-04-16 17:22:32 +0800

    c

---
+++ mmx/mmx/Data/mmxDataBase.cs
@@ -0,0 +1,53 @@
+﻿using mmx.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mmx.Data
+{
+
+    public class mmxDataBase
+    {
+        readonly SQLiteAsyncConnection database;
+
+        public mmxDataBase(string dbPath)
+        {
+            database = new SQLiteAsyncConnection(dbPath);
+            database.CreateTableAsync<Item>().Wait();
+        }
+
+        public Task<List<Item>> GetItemsAsync()
+        {
+            return database.Table<Item>().ToListAsync();
+        }
+
+        public Task<List<Item>> GetItemsNotDoneAsync()
+        {
+            return database.QueryAsync<Item>("SELECT * FROM [TodoItem] WHERE [Done] = 0");
+        }
+
+        public Task<Item> GetItemAsync(string id)
+        {
+            return database.Table<Item>().Where(i => i.Id == id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveItemAsync(Item item)
+        {
+            if (null != item.Id)
+            {
+                return database.UpdateAsync(item);
+            }
+            else
+            {
+                return database.InsertAsync(item);
+            }
+        }
+
+        public Task<int> DeleteItemAsync(Item item)
+        {
+            return database.DeleteAsync(item);
+        }
+    }
+}
---
+++ mmx/mmx/Models/Words.cs
@@ -0,0 +1,27 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mmx.Models
+{
+    public class Words
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        //唯一编号，生成后不变
+        public string Gid { get; set; }
+        //上级编号
+        public string Sid { get; set; }
+        //名称
+        public string Name { get; set; }
+        //内容
+        public string Content { get; set; }
+        //类型：W-单词，S-语句
+        public string Type { get; set; }
+        //上传：S-上传成功，F-上传失败，N-未上传
+        public string Upload { get; set; }
+        //上传时间
+        public DateTime UploadDate { get; set; }
+    }
+}
